52be4008c9020ef7c4847f693c0a0e9756026404
Arindam <arindammahato567@gmail.com>
2022-11-04 07:42:11 +0530

    video streaming app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Box, Button, Heading, Stack, HStack, VStack, Input, Text} from '@chakra-ui/react';
+import { AiOutlineSend } from 'react-icons/ai';
+// import {FaYoutubeSquare, FaInstagramSquare, FaGithubSquare} from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <Box bgColor={'blackAlpha.900'} minH={'40'} p='16' color={'white'}>
+        <Stack direction={['column','row']}>
+            <VStack alignItems={'stretch'} w={'full'} px={'4'} >
+                <Heading size='md' textTransform={'uppercase'} textAlign={['center','left']} >
+                    Subribe to get updates
+                </Heading>
+                <HStack borderBottom={'2px solid white'} py='2' >
+                    <Input placeholder='Enter Email here...' border={'none'} borderRadius={'none'} outline={'none'} focusBorderColor={"none"}/>
+                    <Button
+                    p={'0'}
+                    colorScheme={'purple'}
+                    variant={'ghost'}
+                    borderRadius={'0 20px 20px 0'}>
+                        <AiOutlineSend size={20}/>
+                    </Button>
+                </HStack>
+            </VStack>
+            <VStack w={'full'} borderLeft={['none','1px solid white']} borderRight={['none','1px solid white']}>
+                <Heading textTransform={'uppercase'} textAlign={'center'}>
+                    VIDEO HUB
+                </Heading>
+                <Text>All rights reserved</Text>
+            </VStack>
+            <VStack w={'full'} >
+                <Heading size={'md'} textTransform={'uppercase'} >
+                    Social Media
+                </Heading>
+                <Button target={'blank'} variant={'link'} colorScheme={'white'}>
+                    <a href='https://www.youtube.com'>  Youtube </a>
+                </Button>
+                <Button target={'blank'} variant={'link'} colorScheme={'white'}>
+                    <a href='https://www.youtube.com'>  Instagram</a>
+                </Button>
+                <Button target={'blank'} variant={'link'} colorScheme={'white'}>
+                    <a href='https://www.youtube.com'> Github</a>
+                </Button>
+            </VStack>
+        </Stack>
+    </Box>
+  )
+}
+
+export default Footer;
